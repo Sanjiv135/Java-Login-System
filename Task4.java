3a04cdf17b120f77370e22cbbd46f50103b5a319
sanjiv kanth <147628194+Sanjiv135@users.noreply.github.com>
2025-08-25 12:39:02 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task4 
+{
+    public static void main(String[] args) 
+    {
+        final String USERNAME = "SANJIV"; 
+        final String PASSWORD = "000000"; 
+        int attempts = 0;
+        final int MAX_ATTEMPTS = 3;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (attempts < MAX_ATTEMPTS) 
+        {
+            System.out.print("Enter username: ");
+            String user = sc.nextLine();
+
+            System.out.print("Enter password: ");
+            String pass = sc.nextLine();
+
+            if (user.equals(USERNAME) && pass.equals(PASSWORD)) 
+            {
+                System.out.println("Access Granted!");
+                sc.close();
+                return;
+            } 
+            else 
+            {
+                attempts++;
+                if (attempts < MAX_ATTEMPTS) 
+                {
+                    System.out.println("Invalid credentials. Attempts left: " + (MAX_ATTEMPTS - attempts));
+                }
+            }
+        }
+
+        System.out.println("Try again later.");
+        sc.close();
+    }
+}
